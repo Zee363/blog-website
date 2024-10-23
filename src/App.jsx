9b6bd0f65e35634d755d86d2f7298d3abb580c7c
Zinhle <zinhle.nhlapo31@gmail.com>
2024-10-23 04:58:47 +0200

    Added a footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header.jsx";
 import Navbar from './components/Navbar.jsx';
 import './components/Navbar.css';
 import './App.css';
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <div>
       <Header />
       <Navbar />
+      <Footer/>
     </div>
   );
 }
